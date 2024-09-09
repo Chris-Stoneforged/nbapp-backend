1f1ea7d8798f2859dc6ee4646b88cf22b5e2812c
Christopher Stone <christopher.stone@stoneforgedgames.com>
2024-09-09 20:59:27 +1200

    Reverted score controller route and ScoreValue db table

--- prisma/migrations/20240909081748_added_score_value/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ScoreValue" (
-    "id" TEXT NOT NULL,
-    "score" INTEGER NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "ScoreValue_id_key" ON "ScoreValue"("id");
--- src/routes/adminRouter.ts
+++ src/routes/adminRouter.ts
@@ -2,14 +2,10 @@ import express, { Router } from 'express';
 import { isUserAuthenticated, isAdmin } from '../middleware/auth';
 import { udpateBracket } from '../controllers/bracketController';
 import errorSafe from '../errors/errorHandler';
-import { setScoreValue } from '../controllers/scoreController';
 
 const router: Router = express.Router();
 router
   .route('/update-bracket')
   .post(errorSafe(isUserAuthenticated, isAdmin, udpateBracket));
-router
-  .route('/update-score')
-  .post(errorSafe(isUserAuthenticated, isAdmin, setScoreValue));
 
 export default router;
