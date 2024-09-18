8d136f2d0438460ada901f8420411e97978bcf63
Christopher Stone <christopher.stone@stoneforgedgames.com>
2024-09-18 20:51:39 +1200

    Finished setting up security middleware

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -7,7 +7,7 @@ export default async function securityHeaders(
 ) {
   response.setHeader(
     'Content-Security-Policy',
-    "script-src 'self' object-src 'none' style-src 'self' default-src 'self'"
+    "script-src 'self' object-src 'none' style-src 'self' default-src 'self' frame-ancestors 'self' img-src 'self'"
   );
   response.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
   response.setHeader('Cross-Origin-Resource-Policy', 'same-origin');
@@ -15,6 +15,7 @@ export default async function securityHeaders(
     'Strict-Transport-Security',
     'max-age=15552000 includeSubDomains'
   );
+  response.setHeader('X-Content-Type-Options', 'nosniff');
   response.removeHeader('X-Powered-By');
 
   next();
